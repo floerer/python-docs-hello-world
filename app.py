bc10d50a205fabb717665b71fcd48b3ec1b0f224
floerer <flovlist1@gmail.com>
2025-12-10 10:54:46 +0100

    Add PoC route for subdomain takeover
    Added a new route for the PoC of subdomain takeover.

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/floerer")
+def poc():
+    return "Hello, this is floerer! This is the PoC for subdomain takeover"
